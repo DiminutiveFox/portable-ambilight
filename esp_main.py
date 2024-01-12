add312c09cc982fe50c2c1193507b85f3e6527e6
DiminutiveFox <pawellisek1998@gmail.com>
2024-01-12 20:47:35 +0100

    01.12.2023 - version 0.1

--- esp_main.py
+++ esp_main.py
@@ -1,4 +1,4 @@
-import usys
+import sys
 import uselect
 from machine import Pin
 import machine
@@ -14,7 +14,7 @@ NEO_PIN = 5  # GPIO for neopixel data
 
 np = neopixel.NeoPixel(Pin(NEO_PIN), NUM_PIXELS)
 serialPoll = uselect.poll()
-serialPoll.register(usys.stdin, uselect.POLLIN)
+serialPoll.register(sys.stdin, uselect.POLLIN)
 LED = Pin(6, Pin.OUT)
 IRQ_PIN = Pin(9, Pin.IN, Pin.PULL_UP)
 
@@ -27,7 +27,41 @@ def convert_to_list(color_string):
 def handle_command(command):
     """Returns command to the stream"""
     if command:
-        usys.stdout.buffer.write(command)
+        sys.stdout.buffer.write(command)
+
+
+def create_ambience2(color_string, scale=True, led_span=1):
+    """Converts message to list of colors and writes it to NeoPixel instance \n \n
+        Arguments: \n
+        scale:            determines if color scale is reduced from 0-255 to 0-99
+        channel_length:   length of the channel string (3 by default, 2 if scale is used)\n
+        led_span:         reduces the number of active WS2812B LEDS"""
+
+    color_length = 6 if scale else 9
+    channel_length = 2 if scale else 3
+
+    color_strings = [color_string[i:i + color_length] for i in range(0, len(color_string), color_length)]
+
+    if scale:
+        color_list = [[scale_channel(int(color[i:i + channel_length])) for i in range(0, len(color), channel_length)]
+                      for color in color_strings]
+    else:
+        color_list = [[int(color[i:i + channel_length]) for i in range(0, len(color), channel_length)]
+                      for color in color_strings]
+
+    # print(color_list)
+    for i, color in enumerate(color_list):
+        np[led_span * i] = color if len(color) == 3 else [0, 0, 0]
+    np.write()
+
+
+def scale_channel(channel_value):
+    channel_max_value = 99
+    channel_scaled_max_value = 255
+
+    return int(channel_value*channel_scaled_max_value/channel_max_value)
+
+
 
 
 def create_ambience(message):
@@ -37,33 +71,39 @@ def create_ambience(message):
     np.write()
 
 
+
 def read_serial():
     """Reads the message"""
-    # return usys.stdin.buffer.readline()
-    return usys.stdin.buffer.read(648) if serialPoll.poll(0) else None
+    # return sys.stdin.buffer.readline()
+    return sys.stdin.buffer.readline(652) if serialPoll.poll(0) else None
+    # return sys.stdin.read(648) if serialPoll.poll(0) else None
+    # if serialPoll.poll(0):
+    #     data = ""
+    #     while sys.read
+
 
 def serial_comm():
-    # esp32_baudrate = 115400
+
+    esp32_baudrate = 115400
     # esp32_baudrate = 230400
-    esp32_baudrate = 460800
+    # esp32_baudrate = 460800
 
     # Changing baudrate of the serial to be able to read data faster
-    machine.UART(0, baudrate=esp32_baudrate)
+    # machine.UART(0, baudrate=esp32_baudrate)
 
     while True:
-        # continuously read commands over serial and handle them
         try:
             data = read_serial()
             if data is not None:
-                data = data.decode('utf-8')
+                data = data.decode('utf-8').strip('\n')
+                config = data[:2]
+                color_string = data[2:]
                 # handle_command(data)
+                create_ambience2(color_string, bool(config[0]), int(config[1]))
                 print(data)
-                # color = convert_to_list(data)
-                # create_ambience(color)
-                usys.stdin.buffer.flush()
-                usys.stdout.flush()
+                time.sleep(0.001)
         except Exception as err:
-            # print(err)
+            print(err)
             pass
 
 
--- main.py
+++ main.py
@@ -6,13 +6,10 @@ from screeninfo import get_monitors
 import serial
 import time
 import json
-import random
-import colorthief
 from sklearn.cluster import KMeans
 from collections import Counter
 import dxcam
 import mss
-
 
 def dominant_color(screenshot, k=1):
 
@@ -183,14 +180,20 @@ def color_gen(h_LEDs=18, w_LEDs=36, h=1440, w=2560):
     return l_average_img_color + u_average_img_color + r_average_img_color
 
 
-def color_gen3(h_LEDs=18, w_LEDs=36, h=1440, w=2560):
+def color_gen3(scale=True, led_span=1, h_leds=18, w_leds=36, h=1440, w=2560):
     """ Returns color spectrum of a display's frame """
     s_time = time.time()
 
+    # Defining the number of active leds
+    led_span = 1 if led_span not in [1, 2, 3] else led_span
+    h_leds = int(h_leds / led_span)
+    w_leds = int(w_leds / led_span)
+
+    # Parameters needed for defining the screenshots' areas
     h_divider = 8
     w_divider = 16
 
-    # Defining bounding boxes
+    # Defining bounding boxes for screenshots
     u_box = {"top": 0, "left": 0, "width": w, "height": int(h / h_divider)}
     l_box = {"top": int(h / h_divider), "left": 0, "width": int(w / w_divider), "height": int(h - h / h_divider)}
     r_box = {"top": int(h / h_divider), "left": int(w - w / w_divider), "width": int(w / w_divider), "height": int(h - h / h_divider)}
@@ -204,44 +207,66 @@ def color_gen3(h_LEDs=18, w_LEDs=36, h=1440, w=2560):
         l_screenshot = cv2.resize(l_screenshot, dsize=(int(h/4), int(w/w_divider/4)), interpolation=cv2.INTER_CUBIC)
         r_screenshot = cv2.resize(r_screenshot, dsize=(int(h/4), int(w/w_divider/4)), interpolation=cv2.INTER_CUBIC)
 
-    print(time.time() - s_time)
+    print(f"Time of taking screenshot: {time.time() - s_time}")
 
     h_r, w_r, c_r = r_screenshot.shape
     h_l, w_l, c_l = l_screenshot.shape
     h_u, w_u, c_u = u_screenshot.shape
 
-    # Dividing screenshots to 16 smaller ones and extracting average color of them
-    l_image_list = [l_screenshot[int(n * h_l / h_LEDs):int(n * h_l / h_LEDs + h_l / h_LEDs), :]
-                    for n in range(h_LEDs-1, -1, -1)]
-    l_average_img_color = [np.average(l_image_list[n], axis=(1, 0)) for n in range(h_LEDs)]
-    l_average_img_color = [[str(int(channels[2])), str(int(channels[1])), str(int(channels[0]))]
-                           for channels in l_average_img_color]
-    l_colors = color_string(l_average_img_color)
+    # Dividing screenshots into smaller ones and extracting average color of them
+    l_image_list = [l_screenshot[int(n * h_l / h_leds):int(n * h_l / h_leds + h_l / h_leds), :]
+                    for n in range(h_leds - 1, -1, -1)]
+    l_average_img_color = [np.average(l_image_list[n], axis=(1, 0)) for n in range(h_leds)]
+    if scale:
+        l_average_img_color = [[str(int(scale_channel(channels[2]))), str(int(scale_channel(channels[1]))),
+                                str(int(scale_channel(channels[0])))] for channels in l_average_img_color]
+    else:
+        l_average_img_color = [[str(int(channels[2])), str(int(channels[1])),
+                                str(int(channels[0]))] for channels in l_average_img_color]
+    l_colors = color_string(l_average_img_color, scale)
 
-    u_image_list = [u_screenshot[:, int(n * w_u / w_LEDs):int(n * w_u / w_LEDs + w_u / w_LEDs)] for n in range(w_LEDs)]
-    u_average_img_color = [np.average(u_image_list[n], axis=(1, 0)) for n in range(w_LEDs)]
-    u_average_img_color = [[str(int(channels[2])), str(int(channels[1])), str(int(channels[0]))]
-                           for channels in u_average_img_color]
-    u_colors = color_string(u_average_img_color)
+    u_image_list = [u_screenshot[:, int(n * w_u / w_leds):int(n * w_u / w_leds + w_u / w_leds)] for n in range(w_leds)]
+    u_average_img_color = [np.average(u_image_list[n], axis=(1, 0)) for n in range(w_leds)]
+    if scale:
+        u_average_img_color = [[str(int(scale_channel(channels[2]))), str(int(scale_channel(channels[1]))),
+                                str(int(scale_channel(channels[0])))] for channels in u_average_img_color]
+    else:
+        u_average_img_color = [[str(int(channels[2])), str(int(channels[1])),
+                                str(int(channels[0]))] for channels in u_average_img_color]
+    u_colors = color_string(u_average_img_color, scale)
 
-    r_image_list = [r_screenshot[int(n * h_r / h_LEDs):int(n * h_r / h_LEDs + h_r / h_LEDs), :] for n in range(h_LEDs)]
-    r_average_img_color = [np.average(r_image_list[n], axis=(1, 0)) for n in range(h_LEDs)]
-    r_average_img_color = [[str(int(channels[2])), str(int(channels[1])), str(int(channels[0]))]
-                           for channels in r_average_img_color]
-    r_colors = color_string(r_average_img_color)
+    r_image_list = [r_screenshot[int(n * h_r / h_leds):int(n * h_r / h_leds + h_r / h_leds), :] for n in range(h_leds)]
+    r_average_img_color = [np.average(r_image_list[n], axis=(1, 0)) for n in range(h_leds)]
+    if scale:
+        r_average_img_color = [[str(int(scale_channel(channels[2]))), str(int(scale_channel(channels[1]))),
+                                str(int(scale_channel(channels[0])))] for channels in r_average_img_color]
+    else:
+        r_average_img_color = [[str(int(channels[2])), str(int(channels[1])),
+                                str(int(channels[0]))] for channels in r_average_img_color]
+    r_colors = color_string(r_average_img_color, scale)
 
     # Returning the extracted colors
-    print(time.time()-s_time)
+    print(f"Time of color extraction: {time.time() - s_time}")
     return l_colors + u_colors + r_colors
 
-def color_string(color_list):
+def scale_channel(channel_value):
+    channel_max_value = 255
+    channel_scaled_max_value = 99
 
+    return channel_value*channel_scaled_max_value/channel_max_value
+
+
+
+def color_string(color_list, scale):
+    channel_legth = 3
+    if scale:
+        channel_legth = 2
     for color in color_list:
-        while len(color[0]) < 3:
+        while len(color[0]) < channel_legth:
             color[0] = '0' + color[1]
-        while len(color[1]) < 3:
+        while len(color[1]) < channel_legth:
             color[1] = '0' + color[1]
-        while len(color[2]) < 3:
+        while len(color[2]) < channel_legth:
             color[2] = '0' + color[2]
 
     return ''.join([''.join(sublist) for sublist in color_list])
@@ -263,40 +288,27 @@ def serial_comm(port, baudrate):
     ser.setDTR(False)
     ser.setRTS(False)
     ser.port = port
-    ser.timeout = 0.001
     ser.baudrate = baudrate
     ser.open()
+    scale = 1
+    led_span = 2
 
     try:
-        # ser.write("START\n".encode('utf-8'))
         while True:
+            # Get time for measurement
             start_time = time.time()
-            # color_list = color_gen()
-            # list_size = len(color_list)
-            # color_list1 = str(color_list[0:20]) + '\n'
-            # color_list2 = str(color_list[18:35]) + '\n'
-            # color_list3 = str(color_list[36:53]) + '\n'
-            # color_list4 = str(color_list[54:71]) + '\n'
-            # message = json.dumps(color_gen()).strip(' ') + '\n'
-            # message = str(color_gen()).replace(' ', '') + '\n'
-            message = color_gen3()
+            message = str(scale) + str(led_span) + color_gen3(scale=bool(scale), led_span=led_span) + '\n'
             # Send the command to the ESP32
             print(message)
             ser.write(message.encode('utf-8'))
-            ser.flush()
             time.sleep(0.001)
-            # ser.write(color_list2.encode('utf-8'))
-            # time.sleep(0.001)
-            # ser.write(color_list3.encode('utf-8'))
-            # time.sleep(0.001)
-            # ser.write(color_list4.encode('utf-8'))
-            # Wait for a response
-            response = ser.readline().strip()
-            print("Response from ESP32:", response)
+            # Wait for response
+            # response = ser.readline().strip()
+            # print("Response from ESP32:", response)
 
+            # Print time
             end_time = time.time()
             print(f'Time elapsed: {end_time - start_time}')
-            # time.sleep(5)w
 
     except KeyboardInterrupt:
         ser.close()
@@ -322,14 +334,12 @@ def strip_characters(input_string, characters_to_remove):
 
     return stripped_string
 
+
 if __name__ == "__main__":
     ESP32_name = 'CH340'
-    # ESP32_baudrate = 115400
+    ESP32_baudrate = 115200
     # ESP32_baudrate = 230400
-    ESP32_baudrate = 460800
+    # ESP32_baudrate = 460800
     # ESP32_baudrate = 921600
     ESP32_port = find_port(ESP32_name)
     serial_comm(ESP32_port, ESP32_baudrate)
-    color_list = color_gen3()
-    print(color_list)
-    print(len(color_list))
